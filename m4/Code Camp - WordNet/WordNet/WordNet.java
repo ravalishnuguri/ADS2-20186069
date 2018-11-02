96902ca20325bae691e885ae701f4d39ce511b50
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-11-02 16:56:17 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m4/Code Camp - WordNet/WordNet/WordNet.java
+++ m4/Code Camp - WordNet/WordNet/WordNet.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+/**
+ * Class for word net.
+ */
 public class WordNet {
     Digraph g;
     LinearProbingHashST<String, ArrayList<Integer>> ht;
@@ -7,19 +11,30 @@ public class WordNet {
     int v;
     SAP sap;
     boolean flag = false;
-    // constructor takes the name of the two input files
-    public WordNet(String synsets, String hypernyms) throws Exception{
+    /**
+     * Constructs the object.
+     *
+     * @param      synsets    The synsets
+     * @param      hypernyms  The hypernyms
+     */
+    public WordNet(String synsets, String hypernyms) throws Exception {
         buildht(synsets);
         buildg(hypernyms);
     }
-
-    private void buildg(String hypernyms)throws Exception{
+    /**
+     * method for buildg.
+     *
+     * @param      hypernyms  The hypernyms
+     *
+     * @throws     Exception  { exception_description }
+     */
+    private void buildg(String hypernyms)throws Exception {
         g = new Digraph(v);
         Scanner sc = new Scanner(new File(hypernyms));
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(",");
-            if(tokens.length > 1){
-                for(int i = 1; i < tokens.length; i++){
+            if (tokens.length > 1) {
+                for (int i = 1; i < tokens.length; i++) {
                     g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
                 }
             }
@@ -27,111 +42,152 @@ public class WordNet {
         isrooteddigraph(g);
         iscycle(g);
     }
-    private boolean isflag(){
+    /**
+     * method for a flag.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean isflag() {
         return flag;
     }
-    private void iscycle(Digraph g){
+    /**
+     * method to check a cycle.
+     *
+     * @param      g     { parameter_description }
+     */
+    private void iscycle(Digraph g) {
         DirectedCycle obj = new DirectedCycle(g);
-        if(obj.hasCycle()){
+        if (obj.hasCycle()) {
             System.out.println("Cycle detected");
             flag = true ;
             return;
         }
     }
-    private void isrooteddigraph(Digraph g){
+    /**
+     * method for rooted graph.
+     *
+     * @param      g     { parameter_description }
+     */
+    private void isrooteddigraph(Digraph g) {
         int count = 0;
-        for(int i = 0; i < g.V(); i++){
-            if(g.outdegree(i) == 0){
+        for (int i = 0; i < g.V(); i++) {
+            if (g.outdegree(i) == 0) {
                 count++;
             }
-            if(count>1){
+            if (count>1) {
                 System.out.println("Multiple roots");
                 flag = true ;
                 return;
             }
         }
     }
-
-    private void buildht(String synsets)throws Exception{
+    /**
+     * method for buildht.
+     *
+     * @param      synsets    The synsets
+     *
+     * @throws     Exception  { exception_description }
+     */
+    private void buildht(String synsets) throws Exception {
         ht = new LinearProbingHashST<String, ArrayList<Integer>>();
         ht1 = new LinearProbingHashST<Integer, String>();
         Scanner sc = new Scanner(new File(synsets));
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(",");
             ht1.put(Integer.parseInt(tokens[0]), tokens[1]);
             String[] input = tokens[1].split(" ");
-            for(int i = 0; i < input.length; i++){
-                if(ht.contains(input[i])){
+            for (int i = 0; i < input.length; i++) {
+                if (ht.contains(input[i])) {
                    ArrayList<Integer> list = ht.get(input[i]);
                    list.add(Integer.parseInt(tokens[0]));
                    ht.put(input[i], list);
-                }else{
+                } else {
                     ArrayList<Integer> list = new ArrayList<Integer>();
                    list.add(Integer.parseInt(tokens[0]));
                    ht.put(input[i], list);
                 }
-
             }
             v++;
         }
     }
 
-    // returns all WordNet nouns
-    public Iterable<String> nouns(){
+    /**
+     * returns all WordNet nouns.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<String> nouns() {
         return null;
     }
 
-    // is the word a WordNet noun?
-    public boolean isNoun(String word){
+    /**
+     * Determines if noun.
+     *
+     * @param      word  The word
+     *
+     * @return     True if noun, False otherwise.
+     */
+    public boolean isNoun(String word) {
         return false;
     }
 
-    // distance between nounA and nounB (defined below)
-    public int distance(String nounA, String nounB){
+    /**
+     * distance between nounA and nounB (defined below).
+     *
+     * @param      below  The below
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int distance(String nounA, String nounB) {
         sap = new SAP(g);
         int dist = sap.length(ht.get(nounA), ht.get(nounB));
         return dist;
     }
 
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
-    public String sap(String nounA, String nounB){
+    /**
+     * method for shortest ancestral path.
+     *
+     * @param      nounA  The noun a
+     * @param      nounB  The noun b
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String sap(String nounA, String nounB) {
          sap = new SAP(g);
         String str = "";
         int id = sap.ancestor(ht.get(nounA), ht.get(nounB));
         return ht1.get(id);
     }
-
-    public void print(){
+    /**
+     * method for printing graph.
+     */
+    public void print() {
         System.out.println(g);
     }
 
     // do unit testing of this class
-    public static void main(String[] args){
+    public static void main(final String[] args){
         Scanner sc = new Scanner(System.in);
-        String file1 = "Files"+"\\" + sc.nextLine();
-        String file2 = "Files"+"\\" + sc.nextLine();
+        String file1 = "Files" + "\\" + sc.nextLine();
+        String file2 = "Files" + "\\" + sc.nextLine();
         String input = sc.nextLine();
-        try{
+        try {
             WordNet obj = new WordNet(file1, file2);
-            if(input.equals("Graph")){
-                if(obj.isflag() == false) obj.print();
-            }
-            else if(input.equals("Queries")){
-                while(sc.hasNextLine()){
+            if (input.equals("Graph")){
+                if (obj.isflag() == false) {
+                    obj.print();
+                }
+            } else if (input.equals("Queries")) {
+                while (sc.hasNextLine()) {
                     String[] tokens = sc.nextLine().split(" ");
                     String str = obj.sap(tokens[0], tokens[1]);
                     int dis = obj.distance(tokens[0], tokens[1]);
-                    System.out.println("distance = "+dis+", ancestor = " + str);
+                    System.out.println("distance = " + dis + ", ancestor = " + str);
                 }
             }
-        }catch(Exception e){
-           System.out.println("IllegalArgumentException");;
+        } catch(Exception e) {
+           System.out.println("IllegalArgumentException");
         }
 
     }
 }
-
-
-
-
