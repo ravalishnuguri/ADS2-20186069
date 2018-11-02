dbdb7e0951df90d86fd041f0cc8eb44ffdb76626
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-11-02 16:44:09 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m4/Code Camp - WordNet/WordNet/SAP.java
+++ m4/Code Camp - WordNet/WordNet/SAP.java
@@ -18,3 +18,69 @@
     // do unit testing of this class
     public static void main(String[] args)
 }*/
+import java.util.*;
+
+public class SAP {
+	Digraph digraph;
+	int distance;
+	int ancestor;
+	public SAP(Digraph G){
+		digraph = G;
+		distance = Integer.MAX_VALUE;
+		ancestor = -1;
+	}
+	public int length(int v, int w){
+		ancestor(v, w);
+		if(ancestor != -1){
+			return distance;
+		}return -1;
+
+	}
+	public int ancestor(int v, int w){
+		BreadthFirstDirectedPaths bfsv = new BreadthFirstDirectedPaths(digraph, v);
+		BreadthFirstDirectedPaths bfsw = new BreadthFirstDirectedPaths(digraph, w);
+		for(int i = 0; i < digraph.V(); i++){
+			if(bfsv.hasPathTo(i) && bfsw.hasPathTo(i)) {
+				int tempdist = bfsv.distTo(i) + bfsw.distTo(i);
+				if(tempdist < distance){
+					distance = tempdist;
+					ancestor = i;
+				}
+			}
+		}
+		return ancestor;
+	}
+	public int length(Iterable<Integer> v, Iterable<Integer> w){
+		ancestor(v, w);
+		if(ancestor != -1){
+			return distance;
+		}return -1;
+	}
+	public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
+		for(int i : v){
+			for(int j : w){
+				ancestor(i, j);
+			}
+		}
+		return ancestor;
+	}
+	public static void main(String[] args) throws Exception{
+		// Scanner sc = new Scanner(System.in);
+		// Digraph g = new Digraph(Integer.parseInt(sc.nextLine()));
+		// int ed = Integer.parseInt(sc.nextLine());
+		// for(int i = 0; i < ed; i++){
+		// 	String[] tokens = sc.nextLine().split(" ");
+		// 	// System.out.println(Arrays.toString(tokens));
+		// 	g.addEdge(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]));
+		// }
+
+		// while(sc.hasNextLine()){
+		// 	SAP obj = new SAP(g);
+		// 	int i = Integer.parseInt(sc.nextLine());
+		// 	int j = Integer.parseInt(sc.nextLine());
+		// 	System.out.println("ans  "+ obj.ancestor(i, j));
+		// 	System.out.println("len  "+ obj.length(i, j));
+		// }
+	}
+
+}
