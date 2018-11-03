63b34e3e8a437a5ae18b9ac3b778cda9ea26fdc2
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-11-03 11:38:29 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m6/ADS-2Week-1/Solution.java
+++ m6/ADS-2Week-1/Solution.java
@@ -1,56 +1,107 @@
+
 import java.util.Scanner;
+/**
+ * Class for page rank.
+ */
 class PageRank {
-	
+    /**
+     * object for Digraph.
+     */
+    private Digraph gp;
+    /**
+     * variable initial rank.
+     */
+    private double intitialrank;
+    /**
+     * method to calculate page rank.
+     * O(|E|*I) where |E| is the number of edges and
+     * I is the number of iterations until convergence.
+     *
+     * @param      g1    The g 1
+     */
+   public void pageRank(Digraph g1) {
+        for (int i = 0; i < g1.V(); i++) {
+
+        }
+    }
+    /**
+     * Gets the pr.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     The pr.
+     */
+    double getPR(int v) {
+        return 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int v = 0; v < gp.V(); v++) {
+            s.append(String.format("%d - ", getPR(v)));
+        }
+        return s.toString();
+    }
 }
 
-class WebSearch {
+// class WebSearch {
 
-}
+// }
+
+/**
+ * class for SOlution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int v = Integer.parseInt(s.nextLine());
+        int e;
+        Digraph dg = new Digraph(v);
+        while (s.hasNextLine()) {
+            String[] tokens = s.nextLine().split(" ");
+            int e1 = Integer.parseInt(tokens[0]);
+            int size = tokens.length;
+            if (size > 2) {
+                for(int i = 1; i < tokens.length; i++) {
+                    dg.addEdge(e1, Integer.parseInt(tokens[i]));
+                }
+            } else {
+                dg.addEdge(e1, Integer.parseInt(tokens[1]));
+            }
+        }
+        System.out.println(dg);
+        System.out.println();
+        PageRank pr = new PageRank();
+        pr.pageRank(dg);
+        // Create page rank object and pass the graph object to the constructor
+        System.out.println(pr);
+        // print the page rank object
+        
 
 
-public class Solution {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int v = Integer.parseInt(s.nextLine());
-		int e;
-		Digraph dg = new Digraph(v);
-		while(s.hasNextLine()) {
-			String[] tokens = s.nextLine().split(" ");
-			int e1 = Integer.parseInt(tokens[0]);
-			int size = tokens.length;
-			if (size > 2) {
-				for(int i = 1; i < tokens.length; i++) {
-					dg.addEdge(e1, Integer.parseInt(tokens[i]));
-				}
-			} else {
-				dg.addEdge(e1, Integer.parseInt(tokens[1]));
-			}
-		}
-		System.out.println(dg);
 
-		// read the first line of the input to get the number of vertices
 
-		// iterate count of vertices times 
-		// to read the adjacency list from std input
-		// and build the graph
-		
-		
-		// Create page rank object and pass the graph object to the constructor
-		
-		// print the page rank object
-		
-		// This part is only for the final test case
-		
-		// File path to the web content
-		String file = "WebContent.txt";
-		
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
-		
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
-		
-	}
+        // This part is only for the final test case
+        // File path to the web content
+        // String file = "WebContent.txt";
+        // instantiate web search object
+        // and pass the page rank object and the file path to the constructor
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
+    }
 }
