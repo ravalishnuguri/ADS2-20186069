dec7286cce2c818f935a547475f6ae5d9b489952
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-11-12 17:18:33 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m13/Radix Sorts/LSD.java
+++ m13/Radix Sorts/LSD.java
@@ -1,14 +1,14 @@
 /******************************************************************************
  *  Compilation:  javac LSD.java
  *  Execution:    java LSD < input.txt
- *  Dependencies: StdIn.java StdOut.java 
+ *  Dependencies: StdIn.java StdOut.java
  *  Data files:   https://algs4.cs.princeton.edu/51radix/words3.txt
  *
  *  LSD radix sort
  *
- *    - Sort a String[] array of n extended ASCII strings (R = 256), each of length w.
+ * Sort a String[] array of n extended ASCIIstrings (R = 256),each of length w.
  *
- *    - Sort an int[] array of n 32-bit integers, treating each integer as 
+ *    - Sort an int[] array of n 32-bit integers, treating each integer as
  *      a sequence of w = 4 bytes (R = 256).
  *
  *  Uses extra space proportional to n + R.
@@ -26,24 +26,24 @@
  *  zoo
  *
  ******************************************************************************/
-
 /**
- *  The {@code LSD} class provides static methods for sorting an
- *  array of <em>w</em>-character strings or 32-bit integers using LSD radix sort.
- *  <p>
- *  For additional documentation,
- *  see <a href="https://algs4.cs.princeton.edu/51radix">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * 
+ * Class for lsd.
  */
 public class LSD {
+    /**
+     * variable of int type.
+     */
     private static final int BITS_PER_BYTE = 8;
 
-    // do not instantiate
+    /**
+     * Constructs the object.
+     */
     public LSD() { }
-    public static String[] check;
+    /**
+     * string array.
+     */
+    private static String[] check;
 
    /**
      * Rearranges the array of W-character strings in ascending order.
@@ -51,24 +51,24 @@ public class LSD {
      * @param a the array to be sorted
      * @param w the number of characters per string
      */
-    public static void sort(String[] a, int w) {
+    public static void sort(final String[] a, final int w) {
         int n = a.length;
         check = a;
-        int R = 256;   // extend ASCII alphabet size
+        final int ra = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
-        for (int d = w-1; d >= 0; d--) {
+        for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
 
             // compute frequency counts
-            int[] count = new int[R+1];
+            int[] count = new int[ra + 1];
             for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
             }
 
             // compute cumulates
-            for (int r = 0; r < R; r++) {
-                count[r+1] += count[r];
+            for (int r = 0; r < ra; r++) {
+                count[r + 1] += count[r];
             }
 
             // move data
@@ -84,6 +84,11 @@ public class LSD {
         check = a;
 
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         String result = "[";
         for (int i = 0; i < check.length - 1; i++) {
@@ -99,46 +104,48 @@ public class LSD {
      *
      * @param a the array to be sorted
      */
-    public static void sort(int[] a) {
-        final int BITS = 32;                 // each int is 32 bits
-        final int R = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
-        final int MASK = R - 1;              // 0xFF
-        final int w = BITS / BITS_PER_BYTE;  // each int is 4 bytes
+    public static void sort(final int[] a) {
+        final int bits = 32;                 // each int is 32 bits
+        final int r = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
+        final int mask = r - 1;              // 0xFF
+        final int w = bits / BITS_PER_BYTE;  // each int is 4 bytes
 
         int n = a.length;
         int[] aux = new int[n];
 
         for (int d = 0; d < w; d++) {
-
             // compute frequency counts
-            int[] count = new int[R+1];
+            int[] count = new int[r + 1];
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 count[c + 1]++;
             }
 
             // compute cumulates
-            for (int r = 0; r < R; r++)
-                count[r+1] += count[r];
-
-            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
-            if (d == w-1) {
-                int shift1 = count[R] - count[R/2];
-                int shift2 = count[R/2];
-                for (int r = 0; r < R/2; r++)
-                    count[r] += shift1;
-                for (int r = R/2; r < R; r++)
-                    count[r] -= shift2;
+            for (int k = 0; k < r; k++) {
+                count[k + 1] += count[k];
             }
 
+            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
+            if (d == w - 1) {
+                int shift1 = count[r] - count[r / 2];
+                int shift2 = count[r / 2];
+                for (int k = 0; k < r / 2; k++) {
+                    count[k] += shift1;
+                }
+                for (int k = r / 2; k < r; k++) {
+                    count[k] -= shift2;
+                }
+            }
             // move data
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 aux[count[c]++] = a[i];
             }
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
 }
--- m13/Radix Sorts/Solution.java
+++ m13/Radix Sorts/Solution.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
+/**
+ * class for Solution.
+ */
 final class Solution {
-	private Solution() { }
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		LSD lsd = new LSD();
-		int count = Integer.parseInt(s.nextLine());
-		String[] str = new String[count];
-		int length;
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * main method for Solution class.
+     * Time complexity is O(N).
+     * N is number of lines in input.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        LSD lsd = new LSD();
+        int count = Integer.parseInt(s.nextLine());
+        String[] str = new String[count];
+        int length;
         for (int i = 0; i < count; i++) {
             str[i] = s.nextLine();
             length = str[i].length();
@@ -14,5 +27,5 @@ final class Solution {
         length = str[0].length();
         lsd.sort(str, length);
         System.out.println(lsd);
-	}
+    }
 }
