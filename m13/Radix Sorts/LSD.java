454d4f06b390530128c88f44d60d4840739b5382
ravalishnuguri <ravalishnuguri@msitprogram.net>
2018-11-12 17:01:23 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m13/Radix Sorts/LSD.java
+++ m13/Radix Sorts/LSD.java
@@ -38,13 +38,14 @@
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
-class LSD {
+public class LSD {
     private static final int BITS_PER_BYTE = 8;
 
     // do not instantiate
     public LSD() { }
+    public static String[] check;
 
-   /**  
+   /**
      * Rearranges the array of W-character strings in ascending order.
      *
      * @param a the array to be sorted
@@ -52,6 +53,7 @@ class LSD {
      */
     public static void sort(String[] a, int w) {
         int n = a.length;
+        check = a;
         int R = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
@@ -60,21 +62,35 @@ class LSD {
 
             // compute frequency counts
             int[] count = new int[R+1];
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
+            }
 
             // compute cumulates
-            for (int r = 0; r < R; r++)
+            for (int r = 0; r < R; r++) {
                 count[r+1] += count[r];
+            }
 
             // move data
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
+            }
 
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
+        check = a;
+
+    }
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i < check.length - 1; i++) {
+            result += check[i] + ", ";
+        }
+        result += check[check.length - 1] + "]";
+        return result;
     }
 
    /**
@@ -84,7 +100,7 @@ class LSD {
      * @param a the array to be sorted
      */
     public static void sort(int[] a) {
-        final int BITS = 32;                 // each int is 32 bits 
+        final int BITS = 32;                 // each int is 32 bits
         final int R = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
         final int MASK = R - 1;              // 0xFF
         final int w = BITS / BITS_PER_BYTE;  // each int is 4 bytes
@@ -92,11 +108,11 @@ class LSD {
         int n = a.length;
         int[] aux = new int[n];
 
-        for (int d = 0; d < w; d++) {         
+        for (int d = 0; d < w; d++) {
 
             // compute frequency counts
             int[] count = new int[R+1];
-            for (int i = 0; i < n; i++) {           
+            for (int i = 0; i < n; i++) {
                 int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
                 count[c + 1]++;
             }
@@ -120,33 +136,9 @@ class LSD {
                 int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
                 aux[count[c]++] = a[i];
             }
-
             // copy back
             for (int i = 0; i < n; i++)
                 a[i] = aux[i];
         }
     }
-
-    // /**
-    //  * Reads in a sequence of fixed-length strings from standard input;
-    //  * LSD radix sorts them;
-    //  * and prints them to standard output in ascending order.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     String[] a = StdIn.readAllStrings();
-    //     int n = a.length;
-
-    //     // check that strings have fixed length
-    //     int w = a[0].length();
-    //     for (int i = 0; i < n; i++)
-    //         assert a[i].length() == w : "Strings must have fixed length";
-
-    //     // sort the strings
-    //     sort(a, w);
-    //     // print results
-    //     for (int i = 0; i < n; i++)
-    //         StdOut.println(a[i]);
-    // }
 }
